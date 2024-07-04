8a7420550755e3fb91b31fc0cb646b5ba568ad3e
SSourabh008 <sourabhbanoshi08@gmail.com>
2024-07-04 22:44:53 +0530

    Cart and item adding

--- client/src/components/FoodItem/FoodItem.jsx
+++ client/src/components/FoodItem/FoodItem.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import "./FoodItem.css";
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 const FoodItem = ({id,name,price,description,image}) => {
+    const[itemCount,setItemCount]=useState(0);
+    const {cartItems,setCartItems,addToCart,removeFromCart}=useContext(StoreContext);
   return (
     <div className='food-item'>
         <div className="food-item-img-container">
-            <img src={image} alt="" />
+            <img src={image} alt="" className='food-item-image' />
+            {
+                !cartItems[id]
+                ?<img src={assets.add_icon_white} className='add' onClick={()=>addToCart(id)}/>
+                :<div className='food-item-counter'>
+                    <img src={assets.add_icon_green} onClick={()=>addToCart(id)} alt="" />
+                    <p>{cartItems[id]}</p>
+                    <img src={assets.remove_icon_red} onClick={()=>removeFromCart(id)} alt="" />
+                </div>
+            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
@@ -15,7 +27,7 @@ const FoodItem = ({id,name,price,description,image}) => {
             <p className="food-item-description">
                 {description}
             </p>
-            <p className="food-item price">{price}</p>
+            <p className="food-item-price">$ {price}</p>
         </div>
     </div>
   )
--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -1,11 +1,27 @@
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
-
+import { useState } from "react";
 export const StoreContext=createContext(null);
 
 const StoreContextProvider=(prop)=>{
+    const[cartItems,setCartItems]=useState({});
+    const addToCart=(itemid)=>{
+            if(!cartItems[itemid]){
+                setCartItems((prev)=>({...prev,[itemid]:1}))
+            }
+            else{
+                setCartItems((prev)=>({...prev,[itemid]:prev[itemid]+1}))
+            }
+    }
+    const removeFromCart=(itemid)=>{
+        setCartItems((prev)=>({...prev,[itemid]:prev[itemid]-1}))
+    }
     const contextValue={
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return(
         <StoreContext.Provider value={contextValue}>
